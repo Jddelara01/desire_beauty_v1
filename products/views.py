929daa688abda9f50463e389545c79e12cfb63b3
Jddelara01 <jddelara01@gmail.com>
2023-10-11 10:45:55 +0000

    add delete product functionality

--- products/views.py
+++ products/views.py
@@ -76,9 +76,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added a product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please make sure the form is valid')   
     else: 
@@ -94,7 +94,7 @@ def add_product(request):
 
 def edit_product(request, product_id):
     """ Edit a product in the store """
-    
+
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -115,3 +115,12 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
