9799936ce1c3a0b35a204df96d73a998fd9926fe
Jddelara01 <jddelara01@gmail.com>
2023-10-14 08:53:49 +0000

    ready for deploment

--- desire_beauty/settings.py
+++ desire_beauty/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 if os.path.isfile('env.py'):
@@ -30,7 +31,7 @@ SECRET_KEY = 'django-insecure-!$o6#7r+6j%_efcsvubwu9^g-btv39tj46g*!7*((8+*$+f!jn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-jddelara01-desirebeauty-10yzwwrlpp9.ws-eu104.gitpod.io', '8000-jddelara01-desirebeauty-10yzwwrlpp9.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['boutique-desire-a7385b96b2ab.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +124,17 @@ WSGI_APPLICATION = 'desire_beauty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
